c98a7c9fc8aef430f91e23b591ece61846a4d7dc
root <mikanmuchiri@gmail.com>
2021-06-17 13:29:28 -0700

    New program file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-  puts("\"Programming is like building a multilingual puzzle \n");
-  return 0;
-  }
+/*This main function utilized puts() function */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle \n");
+return 0;
+}
