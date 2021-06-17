3a94e6c05af12e2f42d1196c8fb2f3ce80cdbcec
root <mikanmuchiri@gmail.com>
2021-06-17 14:00:01 -0700

    New program file added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*Function to print using puts()*/
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *This main function utilized puts() function
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
