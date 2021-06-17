f6617bed114303552c3e653e22d1cb4dc1c05a4d
root <mikanmuchiri@gmail.com>
2021-06-17 13:08:02 -0700

    New program file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main{
-  printf("\"Programming is like building a multilingual puzzle \n")
+  puts("\"Programming is like building a multilingual puzzle \n");
+  return 0;
