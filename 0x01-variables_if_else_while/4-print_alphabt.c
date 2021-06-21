152ab4bd3c300294768f23159d909662bba41791
root <mikanmuchiri@gmail.com>
2021-06-21 01:52:36 -0700

    4-alphabt.c added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main- begin of program
+ * Description: printd out the alphabets in
+ * lowercase character alphabets
+ * Return: exit 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' || ch != 'e')
+		{
+			putchar(ch);
+		}
+
+			ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
