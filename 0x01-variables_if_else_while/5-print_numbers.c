7b45da77f71ea01c4971311614a52f04b47d6bc7
root <mikanmuchiri@gmail.com>
2021-06-21 04:56:16 -0700

    5 program added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry of program
+ *Description: Prints 0-9 numbers
+ *Return: exit 1
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf(num);
+		num++;
+	}
+	return (0);
+}
