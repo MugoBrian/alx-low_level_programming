b00c4b060450178a0d307aeba14929754ffe03f8
root <mikanmuchiri@gmail.com>
2021-06-21 06:11:13 -0700

    9-print_comb.c added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *main -start
+ *Description:a program that prints all
+ *possible combinations of
+ *single-digit numbers.
+*Return: exit 0'
+*/
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
