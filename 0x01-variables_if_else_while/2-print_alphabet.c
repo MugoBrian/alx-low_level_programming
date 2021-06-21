c71ae6183d0f1d2d1b1d5636e57e8dc73fb2114b
root <mikanmuchiri@gmail.com>
2021-06-21 00:10:42 -0700

    New program added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main- begin of program
+ * Description: printd out the alphabets in
+ * lowercase character alphabets
+ * Return: exit 0
+ */
+int main(void)
+{
+	ch al = 'abcdefghijklmnopqrstuvwxyz'
+
+		putchar(al)
+		return (0);
+}
+
