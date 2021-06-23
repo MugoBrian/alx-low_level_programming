0834299f96041462b9d47f3c29fcdad8e0bce2a2
root <mikanmuchiri@gmail.com>
2021-06-23 07:42:54 -0700

    New dir and files added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - prints alphabets
+ *Description: prints all alphabets and a newline
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
+/**
+ *main - entry of program
+ *Description: prints alphabets
+ *Return:0
+ */
+int main(void)
+{
+	ptint_alphabet(void);
+	return (0);
+}
