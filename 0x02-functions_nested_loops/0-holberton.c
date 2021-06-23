8e2072a992fdc7e251ebefbf11b06da487ef2790
root <mikanmuchiri@gmail.com>
2021-06-23 06:56:26 -0700

    New dir and files added

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <uistd.h>
+#include <holberton.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,10 +7,6 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int main(void)
 {
 	char hol = "Holberton";
