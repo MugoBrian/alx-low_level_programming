53e3536277e643f3eb79c5ef31a3ef2efc8d609d
root <mikanmuchiri@gmail.com>
2021-06-26 03:38:44 -0700

    New dir added and functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_isupper -checks if a character is alphabet
+ *Description: if alphbaet is upper return 1
+ *@ch: parameter to evaluate
+ *Return: 1 if its uppercase,0 otherwise
+ */
+
+int _isupper(int ch)
+{
+	if ((ch >= 'A') && (ch <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
