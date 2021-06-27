62f8a16906931cdddedef370903f611a7811ee1b
root <mikanmuchiri@gmail.com>
2021-06-27 14:46:58 -0700

    File updated

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_isdigit - checks if its a number
+ *Description: the functions checks a number btn  *0-9
+ *@c: parameter to be evaluated
+ *Return:1 if a number ,0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+		return (1);
+	else
+		return (0);
+}
