1b15a93b8274174da152756a95da0b2339910239
MugoBrian <mikanmuchiri@gmail.com>
2021-07-05 19:23:14 +0300

    3-strspn.c corrected

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *_strspn - calculates number of bytes
+ *Description:Gets the lenght of a prefix substring.
+ *@s:The array of the string
+ *@accept:The prefix substring
+ *Return: number of bytes in the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i;
+int total = 0;
+
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+total++;
+break;
+}
+else if (accept[i + 1] == '\0')
+{
+return (total);
+}
+s++;
+}
+return (total);
+ }
