a62559c1d45dd528d426004980eb75fb81df3ba4
MugoBrian <mikanmuchiri@gmail.com>
2021-07-05 18:23:40 +0300

    1-memcpy.c added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_memset() - function fills first
+ *Description:The function fills the first n bytes of the memory area pointed
+ *to by s with the constant byte b.
+ *@s:A pointer to the memory area with
+ *@b:The character to fill in the memoryh area with
+ *@n:The number of bytes to be filled.
+ *Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, usigned int n)
+{
+  unsigned int i;
+
+  for (i = 0; i < n; i++)
+  {
+    s[i] = b;
+  }
+  return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_memcpy -copies n bytes from src to dest
+ *Description:The function copies n bytes from src to memory area dest
+ *@dest:The destination memory area
+ *@src:The source memory area
+ *@n:the bytes
+ *Return:a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+src[i] = dest[i];
+return (dest);
+}
