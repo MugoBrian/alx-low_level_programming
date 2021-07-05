3b0d6f793113dd2f75964ab85261d205b1e1a291
MugoBrian <mikanmuchiri@gmail.com>
2021-07-05 18:54:38 +0300

    2-strchr.c added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 
 /**
- *_strchr - searches for occurence in a string
+ *_strchr -searches for occurence in a string
  *Description:The first occurrence of the character c in the string s or NULL
  *@c:character to be searched in a string
  *@s:the string array
+ *Return:pointer to c or Null if otherwise
  */
 
 char *_strchr(char *s, char c)
 {
 int i;
-for (i = 0; i < s; i++)
+
+for (i = 0; s[i] >= '\0'; i++)
 {
 if (s[i] == c)
 {
