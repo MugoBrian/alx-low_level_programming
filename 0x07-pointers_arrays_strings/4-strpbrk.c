267654eafa258b2ebd192e1b6add0fc49af6c980
MugoBrian <mikanmuchiri@gmail.com>
2021-07-07 11:52:55 +0300

    4-strpbrk.c added

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *_strpbrk - locates first occurrence in string s
+ *Description: locates the first occurrence in strings of any bytes in string\
+ *accept
+ *@s:the whole string
+ *@accept:string to look for
+ *Return: byte in s in accept otherwise null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+char bytes;
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+return (bytes);
+}
+s++;
+}
+return ('\0');
+}
