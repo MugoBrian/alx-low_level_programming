afbfbcc413c9b3af5b9b4d26f2e3e5837c5de8cd
MugoBrian <mikanmuchiri@gmail.com>
2021-07-07 14:15:42 +0300

    100-set_string.c added

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *set_string - set value of pointer
+ *Description: a function that sets the value of a pointer to a char
+ *@s:pointer to set
+ *@to: to be reseted
+ *Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
