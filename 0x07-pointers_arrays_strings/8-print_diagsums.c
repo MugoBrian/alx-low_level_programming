843e309a92ce7028b19c115ef0dc8f7e805d80a2
MugoBrian <mikanmuchiri@gmail.com>
2021-07-07 13:52:37 +0300

    Diagsums.c corrected

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,6 +11,7 @@ void print_diagsums(int *a, int size)
 {
 int i;
 int total1 = 0, total2 = 0;
+
 for (i = 0; i < size; i++)
 {
 total1 += a[i];
@@ -22,6 +23,6 @@ for (i = 0; i < size; i++)
 total2 += a[i];
 a -= size;
 }
-printf("%d\n",total1,total2);
+printf("%d,%d\n", total1, total2);
 }
 
