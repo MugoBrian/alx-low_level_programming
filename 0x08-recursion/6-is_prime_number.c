07bb88944100b8eb8bc3657a8f385ec9ed632aef
MugoBrian <mikanmuchiri@gmail.com>
2021-07-08 18:01:10 +0300

    6-is_prime_number.c added

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,68 +1,46 @@
 #include "holberton.h"
 
 int is_prime_number(int n);
-int sqrt(int x, int i);
-int prime(int n, int d);
+int is_divisible(int n, int d);
 /**
- *is_prime_number - function of prime number
- *Description: a function that returns 1 if integer is a prime number
+ *is_divisible  - checks a number is divisible
+ *Description: a function that checks a number is divisible
  *@n: number
- *Return: 1 if integer a prime number else 0 otherwise
+ *@d:divisor
+ *Return: 0 is divisible and -1 if not
  */
 
-int is_prime_number(int n)
+int is_divisible(int n, int d)
 {
-if (n < 2)
+if (n % d ==  0)
 {
 return (0);
 }
-if (n < 4)
+if (d == num / 2)
 {
 return (1);
 }
-return (prime(n, 2));
+return (is_divisible(n, d + 1));
 }
 
 /**
- *sqrt - return the square root of number
- *@x:number
- *@i:number incrementer
- *Return: square root of x
- */
-
-int sqrt(int x, int i)
-{
-int sq;
-
-sq = i * i;
-if (sq >= x)
-{
-return (i);
-}
-else
-{
-return (sqrt(x, i + 1));
-}
-}
-/**
- *prime - helper function that determines the prime number
+ *is_prime_number - helper function that determines the prime number
  *Description: determines if its a prime number
  *@n: number to test
  *@d: number increment
  *Return: returns 1
  */
-int prime(int n, int d)
+int is_prime_number(int n, int d)
 {
-if (n % d == 0)
+int d = 2;
+
+if (n <= 1)
 {
 return (0);
 }
-else if (sqrt(n, 1) < d)
+if (n >= 2 && n <= 3)
 {
 return (1);
 }
-else
-{
-return (prime(n, d + 1));
-}
+return (is_divisible(n, d));
 }
