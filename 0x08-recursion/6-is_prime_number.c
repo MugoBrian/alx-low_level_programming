eb5b3e021dde7a3a864187ac62fa964dfc117db3
MugoBrian <mikanmuchiri@gmail.com>
2021-07-08 17:00:48 +0300

    6-is_prime_number.c added

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,68 @@
+#include "holberton.h"
+
+/**
+ *is_prime_number - function of prime number
+ *Description: a function that returns 1 if integer is a prime number
+ *@n: number
+ *Return: 1 if integer a prime number else 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+int i;
+if (n < 2)
+{
+return (0);
+}
+if (n < 4)
+{
+return (1);
+}
+else
+{
+return (prime(n, 1));
+}
+}
+
+/**
+ *sqrt - return the square root of number
+ *@x:number
+ *@i:number incrementer
+ *Return: square root of x
+ */
+
+int sqrt(int x, int i)
+{;
+int sq;
+sq = i * i;
+if (sq >= x)
+{
+return (i);
+}
+else
+{
+return (sqrt(x, i + 1));
+}
+}
+/**
+ *prime - helper function that determines the prime number
+ *Description: determines if its a prime number
+ *@n: number to test
+ *@d: number increment
+ *Return: returns 1
+ */
+int prime(int n, int d)
+{
+if (x % d == 0)
+{
+return (0);
+}
+else if (sqrt(n, 1) < d)
+{
+return (1);
+}
+else
+{
+return (prime(n, d + 1));
+}
+}
