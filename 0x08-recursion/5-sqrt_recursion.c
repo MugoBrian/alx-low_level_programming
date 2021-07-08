ce98ec7e67bebf3c78ef8178cd9672d9d74c9534
MugoBrian <mikanmuchiri@gmail.com>
2021-07-08 15:12:46 +0300

    5-sqrt_recursion added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ *_sqrt_recursion - returns the natural square root
+ *Description: function that returns the natural square root of a number
+ *@n:natural number
+ *Return:n does not have a natural square root, return -1
+ */
+
+int _sqrt_recursion(int n)
+{
+return (sq(n, 1));
+}
+
+/**
+ *sq - helper function to solve _sqrt_recursion
+ *@c: number to determine to the square
+ *@i: incrementer
+ *Return: square root if natural number
+ */
+
+int sq(int c, int i)
+{
+int sqre = i * i;
+if (sqre == c)
+{
+return (i);
+}
+else if (sq < c)
+{
+return (c, sq(c, i + 1));
+}
+else
+{
+return (-1);
+}
+}
+
