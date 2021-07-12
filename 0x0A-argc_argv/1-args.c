e46f1d27a31876e8e131f1b0dc20c48bfcc61c3f
MugoBrian <mikanmuchiri@gmail.com>
2021-07-12 23:18:01 +0300

    1-args.c program added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - program that prints the number of arguments
+ *Description: should print a number followed by a new line.
+ *it will print the new name.
+ *@argc:argument count
+ *@argv:argument vector
+ *Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+
+printf("%d\n", argc);
+
+return (0);
+}
