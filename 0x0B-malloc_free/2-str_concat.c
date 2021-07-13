033235ea0b86e0e57a3106fe062e57a008c5e129
MugoBrian <mikanmuchiri@gmail.com>
2021-07-14 00:08:12 +0300

    2-str_concat added

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *str_concat - returns pointer of contents of s1 followed by s2.
+ *Description: The returned pointer should point to a newly allocated space 
+ *in memory which contains the contents of s1, followed by the contents of s2, 
+ *and null terminated
+ *@s1: string 1
+ *@s2: string 2
+ *Return: null on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *p;
+int i;
+int j = 0;
+int con = 0;
+
+
+if (s1 && s2 == NULL)
+{
+s2 = "";
+s1 = "";
+}
+for (i = 0; s1[i] || s2[i]; i++)
+{
+con++;
+}
+p = malloc(sizeof(char) * con);
+if (p == NULL)
+{
+return (NULL);
+}
+for (i = 0; s1[i]; i++)
+{
+p[j++] = s1[i];
+}
+for (i = 0; s2[i]; i++)
+{
+p[j++] =  s2[i];
+}
+
+return (p);
+}
