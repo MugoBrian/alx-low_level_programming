9e94a1ac38ed7f8e9b743fc7f4cd559adab86d63
MugoBrian <mikanmuchiri@gmail.com>
2021-07-18 16:38:14 +0300

    New files and directory added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *malloc_checked - allocates memory
+ *Description: allocates memory using malloc function and returns a pointer
+ *@b:bytes 
+ *Return: if fails returns 98 value else return a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+void *p = malloc(b);
+if (!p)
+{
+exit(98);
+}
+return (p);
+}
