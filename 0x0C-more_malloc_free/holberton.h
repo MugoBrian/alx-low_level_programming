b29e6d5c1ae90e327d655544b54dc3a15ea71cb4
MugoBrian <mikanmuchiri@gmail.com>
2021-07-18 17:54:08 +0300

    New files added

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,15 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int onlyNumbers(char *c);
-void multiply(char* s1, char* s2);
-char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char*s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _strlen(char *s);
 
 #endif /*HOLBERTON_H */
