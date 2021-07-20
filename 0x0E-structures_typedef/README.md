2f48527a2e3f5188e445d9cb0a886647bfc642ee
MugoBrian <mikanmuchiri@gmail.com>
2021-07-20 14:27:10 +0300

    New dir and files added

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+0x0E-structures_typedef
+
+This directory contains the structs data type and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog_h
+#define dog_h
+/**
+ *struct dog - defines a new type struct dog
+ *@name:name of dog
+ *@age:age of dog
+ *@owner: owner of dog
+ *
+ *Description: defines new type struct dog with name,age and owner variable
+ */
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+int _strlen(char *s);
+
+#endif /*dog_h*/
